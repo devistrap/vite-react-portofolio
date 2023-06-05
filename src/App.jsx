d3153288b1268665f6e9209e995a6955c2cc18f1
bart nieman <bart.nieman@student.rocva.nl>
2023-06-05 19:19:42 +0200

    installing tailwind and creating the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import * as React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 50
+    };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  addOne = () => {
+    const { count } = this.state;
+    if (count === 100) {
+      console.info("max reached");
+    } else if (count < 100) {
+      this.setState({ count: count + 1 });
+    }
+  }
+
+  removeOne = () => {
+    const { count } = this.state;
+    if (count === 0) {
+      console.info("min reached");
+    } else if (count > 0) {
+      this.setState({ count: count - 1 });
+    }
+  }
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div className="App">
+        <h1>progress bar</h1>
+        <input type='range' min="0" max="100" value={count} id='test'  />
+        <button onClick={this.addOne} >add 1</button>
+        <button onClick={this.removeOne}>remove 1</button>
+        <p >{count}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
