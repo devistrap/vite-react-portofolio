3fbdf63d6b7f57f6aaf07fcb7573d33694e694d4
bart nieman <bart.nieman@student.rocva.nl>
2023-06-07 12:48:55 +0200

    basic slider

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ class App extends React.Component {
     const { count } = this.state;
     if (count === 100) {
       console.info("max reached");
+      this.setState({ count: "max reached" });
     } else if (count < 100) {
       this.setState({ count: count + 1 });
     }
@@ -25,20 +26,28 @@ class App extends React.Component {
     const { count } = this.state;
     if (count === 0) {
       console.info("min reached");
+      this.setState({ count: "min reached" });
     } else if (count > 0) {
-      this.setState({ count: count - 1 });
+      this.setState({ count: count - 1});
     }
+  }
+  reset = () => {
+    const { count } = this.state;
+      this.setState({ count: 50});
   }
 
   render() {
     const { count } = this.state;
     return (
-      <div className="App">
-        <h1>progress bar</h1>
-        <input type='range' min="0" max="100" value={count} id='test'  />
-        <button onClick={this.addOne} >add 1</button>
-        <button onClick={this.removeOne}>remove 1</button>
-        <p >{count}</p>
+      <div className="App border-gray-900 border-2 w-1/2 flex justify-center flex-col ">
+        <h1 className='my-5 text-2xl'>progress bar</h1>
+        <input type='range' min="0" max="100" value={count} id='test' className=' ml-10 h-4 bg-gray-300 rounded-full appearance-none'  />
+        <div>
+        <button onClick={this.addOne} className='bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>add 1</button>
+        <button onClick={this.removeOne} className='bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>remove 1</button>
+        <button onClick={this.reset} className='bg-red-500 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>reset</button>
+        </div>
+        <p className='w-1/2 absolute top-15 left-3'>{count}</p>
       </div>
     );
   }
