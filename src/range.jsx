d583e788e70c0d521cc08e3614e10f9935bde8f5
bart nieman <bart.nieman@student.rocva.nl>
2023-06-09 11:48:53 +0200

    .

--- src/range.jsx
+++ src/range.jsx
@@ -42,6 +42,7 @@ class Range extends React.Component {
   reset = () => {
     const { count } = this.state;
     this.setState({count: 50})
+    document.getElementById("score").style.display="None"
     
   }
 
@@ -51,17 +52,15 @@ class Range extends React.Component {
       
       <div className="App border-gray-900 border-2 w-1/3 flex justify-stretch flex-col ">
           <h1 className='my-5 text-2xl'>progress bar  "input range"</h1>
-          
-
-          <input type='range' min="0" max="100" value={count} id='test' className=' ml-10 h-4 rounded-full'  />
-          <button onClick={this.submit} name='submit' className='bg-red-500 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>submit</button>
+          <input type='range' min="0" max="100" value={count} id='test' className=' ml-10 h-4 font-extrabold '  />
+          <p className='text-2xl inline'>{count}</p><button onClick={this.submit} name='submit' className='bg-red-500 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>submit</button>
           <div>
             <button onClick={this.addOne} className='bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>+1</button>
             <button onClick={this.removeOne} className='bg-blue-500 hover:bg-blue-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>-1</button>
             <button onClick={this.reset} className='bg-red-500 hover:bg-red-700 text-white font-bold mx-5 py-2 px-4 rounded-full  my-5 w-1/4'>reset</button>
           </div>
           
-          <p className='w-1/2 absolute top-16 left-3'>{count}</p>
+          
           <p id='score'>de waarde van de schuifbar is {this.state.count1}</p>
 
       
